383113dafcb816a827754356955fc6ea51a5208e
unknown <amrsalam@hotmail.com>
2023-12-17 12:42:09 +0200

    Calculate average order amount for employee Endpoint

---
+++ RestaurantReservation.API/Controllers/EmployeeOrdersController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RestaurantReservation.Db.Services;
+
+namespace RestaurantReservation.API.Controllers
+{
+    [Route("api/employees/{employeeId}")]
+    [ApiController]
+    public class EmployeeOrdersController : ControllerBase
+    {
+        private readonly IEmployeeService _employeeService;
+
+        public EmployeeOrdersController(IEmployeeService employeeService)
+        {
+            _employeeService = employeeService;
+        }
+
+        [HttpGet("average-order-amount")]
+        public ActionResult<double> CalculateAverageOrderAmount(int employeeId)
+        {
+            return Ok(_employeeService.CalculateAverageOrderAmount(employeeId));
+        }
+    }
+}
