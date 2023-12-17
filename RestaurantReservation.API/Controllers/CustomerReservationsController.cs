0f868e2954bfc66931790956c175be3f428e1946
unknown <amrsalam@hotmail.com>
2023-12-17 11:39:52 +0200

    Retrieve Reservations by Customer Endpoint

---
+++ RestaurantReservation.API/Controllers/CustomerReservationsController.cs
@@ -0,0 +1,38 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using RestaurantReservation.API.Models;
+using RestaurantReservation.Db.Services;
+
+namespace RestaurantReservation.API.Controllers
+{
+    [Route("api/reservations/customer")]
+    [ApiController]
+    public class CustomerReservationsController : ControllerBase
+    {
+        private readonly IReservationService _reservationService;
+        private readonly IMapper _mapper;
+        private readonly ICustomerService _customerService;
+
+        public CustomerReservationsController(IReservationService reservationService, IMapper mapper, ICustomerService customerService)
+        {
+            _reservationService = reservationService ?? throw new ArgumentNullException(nameof(reservationService));
+            _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
+            _customerService = customerService ?? throw new ArgumentNullException(nameof(customerService));
+        }
+
+        [HttpGet("{customerId}")]
+        public async Task<ActionResult<List<ReservationDTO>>> GetCustomerReservationsAsync(int customerId)
+        {
+            var customerExists = await _customerService.CustomerExists(customerId);
+
+            if (!customerExists)
+            {
+                return NotFound();
+            }
+
+            var reservations = await _reservationService.GetReservationsByCustomerAsync(customerId);
+
+            return Ok(_mapper.Map<List<ReservationDTO>>(reservations));
+        }
+    }
+}
