f9cd3e6961dc277041c1929bb63e76f64840c311
unknown <amrsalam@hotmail.com>
2023-12-17 13:00:33 +0200

    Implementing orders and menu items Endpoints

---
+++ RestaurantReservation.API/Controllers/OrderController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RestaurantReservation.Db.Models;
+using RestaurantReservation.Db.Services;
+
+namespace RestaurantReservation.API.Controllers
+{
+    [Route("api/reservations/{reservationId}")]
+    [ApiController]
+    public class OrderController : ControllerBase
+    {
+        private readonly IReservationService _reservationService;
+
+        public OrderController(IReservationService reservationService)
+        {
+            _reservationService = reservationService;
+        }
+
+        [HttpGet("orders")]
+        public async Task<ActionResult<List<Orders>>> GetOrderList(int reservationId)
+        {
+            var reservationExists = await _reservationService.ResevationExists(reservationId);
+
+            if (!reservationExists) 
+            {
+                return NotFound();
+            }
+
+            return Ok(await _reservationService.ListOrdersAndMenuItemsAsync(reservationId));
+        }
+
+        [HttpGet("menu-items")]
+        public async Task<ActionResult<List<MenuItems>>> GetMenuItemList(int reservationId)
+        {
+            var reservationExists = await _reservationService.ResevationExists(reservationId);
+
+            if (!reservationExists)
+            {
+                return NotFound();
+            }
+
+            return Ok(await _reservationService.ListOrderedMenuItemsAsync(reservationId));
+        }
+    }
+}
