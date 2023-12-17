b27291513436108a9c6bf807dfd31b85fb9b38bb
unknown <amrsalam@hotmail.com>
2023-12-17 10:34:59 +0200

    List managers Endpoint

---
+++ RestaurantReservation.API/Controllers/ManagerController.cs
@@ -0,0 +1,29 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using RestaurantReservation.API.Models;
+using RestaurantReservation.Db.Services;
+
+namespace RestaurantReservation.API.Controllers
+{
+    [Route("api/employees/managers")]
+    [ApiController]
+    public class ManagerController : ControllerBase
+    {
+        private readonly IEmployeeService _employeeService;
+        private readonly IMapper _mapper;
+
+        public ManagerController(IEmployeeService employeeService , IMapper mapper)
+        {
+            _employeeService = employeeService ?? throw new ArgumentNullException(nameof(employeeService));
+            _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper)); ;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<EmployeeDTO>>> ListManagersAsync()
+        {
+            var managersList = await _employeeService.ListManagersAsync();
+
+            return Ok(_mapper.Map<List<EmployeeDTO>>(managersList));
+        }
+    }
+}
